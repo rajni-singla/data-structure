9107fa5b489dc919a50612224cc30287a13ef7a0
rajni-singla <rajni-singla@users.noreply.github.com>
2015-08-22 19:39:51 +0530

    Create is.c

---
+++ is.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int i,j,*a,n,temp,time1,time2,time3;
+	printf("enter no. of elements u want to sort\n");
+	scanf("%d",&n);
+	a=(int *)malloc(n*sizeof (int));
+	printf("enter elements\n");
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	struct timeval t1,t2;
+	printf("time before sorting\n");
+	gettimeofday(&t1,NULL);
+	time1=t1.tv_sec*1000000L+t1.tv_usec;
+	printf("%d\n",time1);
+	for(i=0;i<n;i++)
+	{	
+		temp=a[i];
+		j=i-1;	
+		while(a[j]>temp&&j>=0)
+		{
+			a[j+1]=a[j];
+			j--;
+		}
+		a[j+1]=temp;
+	}
+	printf("after sorting\n");
+	for(i=0;i<n;i++)
+	printf("%d\n",a[i]);
+	gettimeofday(&t2,NULL);
+	time2=t2.tv_sec*1000000L+t2.tv_usec;
+	printf("time after sorting\n");
+	printf("%d\n",time2);
+	time3=time2-time1;
+	printf("time taken in sorting\n");
+	printf("%d\n",time3);
+}
